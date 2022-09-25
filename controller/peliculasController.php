118e1bf352ee569402648fa35b800bb383d43429
Leonardo200298 <leonardosanchez119@gmail.com>
2022-09-25 17:15:38 -0300

    Feat:add MVC

--- controller/peliculasController.php
+++ controller/peliculasController.php
@@ -3,5 +3,11 @@
 
 
 class peliculasController{
-    
+    private $model;
+    private $view;
+    function __construct()
+    {
+        $this->model = new peliculasModel();
+        $this->view = new peliculasView();
+    }
 }
