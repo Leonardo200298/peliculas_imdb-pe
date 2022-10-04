94ff68c6312f81afd654b2e55410234def80c333
Leonardo200298 <leonardosanchez119@gmail.com>
2022-10-04 11:21:58 -0300

    Feat:add new folders, I get permissions to templates_c, add new template

---
+++ templates_c/2e1c47af02fb166ae72e2e951fd58a48892446c7_0.file.table.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 4.2.1, created on 2022-10-04 16:17:42
+  from '/opt/lampp/htdocs/carpeta/leo-imdb/templates/table.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.2.1',
+  'unifunc' => 'content_633c40865aabf1_67096892',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2e1c47af02fb166ae72e2e951fd58a48892446c7' => 
+    array (
+      0 => '/opt/lampp/htdocs/carpeta/leo-imdb/templates/table.tpl',
+      1 => 1664893057,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_633c40865aabf1_67096892 (Smarty_Internal_Template $_smarty_tpl) {
+?><h1><?php echo $_smarty_tpl->tpl_vars['peliculas']->value;?>
+</h1><?php }
+}
--- view/peliculasView.php
+++ view/peliculasView.php
@@ -1,9 +1,15 @@
 <?php
-require_once './librerias/smarty-4.2.1/libs/Smarty.class.php';
+require_once './libs/smarty-4.2.1/libs/Smarty.class.php';
 
 class peliculasView{
+    private $smarty;
+    function __construct()
+    {
+        $this->smarty = new Smarty();
+        
+    }
+
     function mostrarPeliculas($movies){
-        $smarty = new Smarty();
    /*      echo "<ul>";
         foreach ($movies as $pelicula) {
             echo "<li>$pelicula->nombre</li> . <li>$pelicula->año .
@@ -11,8 +17,8 @@ class peliculasView{
             </li>";
         }
     echo "</ul>"; */
-    $smarty->assign('peliculas', "Todas las peliculas");
-    $smarty->display('templates/table.tpl'); // muestro el template   
+    $this->smarty->assign('peliculas', "Todas las peliculas");
+    $this->smarty->display('templates/table.tpl'); // muestro el template   
 
     }
 
