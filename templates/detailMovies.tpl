e8dc9a06c675ad40d5550d42986f47fe8ebe1012
Leonardo200298 <leonardosanchez119@gmail.com>
2022-10-06 12:05:18 -0300

    Feat:details of movies finalized

--- templates/detailMovies.tpl
+++ templates/detailMovies.tpl
@@ -1,8 +1,37 @@
 <h1>{$detalles}</h1>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+
 <ul class="list-group">
-   <li>{$moviesDetail->nombre}</li>
-   <li>{$moviesDetail->anio}</li>
-   <li>{$moviesDetail->produccion}</li>
-   <li>{$moviesDetail->recaudacion}</li>
+{foreach from=$details item=$detail}
+      <li class="list-group-item list-group-item-success">Nombre: {$detail->nombre}</li>
+      <li class="list-group-item list-group-item-success">Fecha de lanzamiento: {$detail->anio}</li>
+      <li class="list-group-item list-group-item-success">Costo de produccion: {$detail->produccion}</li>
+      <li class="list-group-item list-group-item-success">Recaudacion: {$detail->recaudacion}</li>     
+       
+{/foreach}
 </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- templates_c/32f79100c4c860cbb4be3dd29f634561743939a4_0.file.detailMovies.tpl.php
+++ templates_c/32f79100c4c860cbb4be3dd29f634561743939a4_0.file.detailMovies.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-10-06 15:50:45
+/* Smarty version 4.2.1, created on 2022-10-06 17:03:43
   from '/opt/lampp/htdocs/carpeta/leo-imdb/templates/detailMovies.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_633edd351bb209_52858770',
+  'unifunc' => 'content_633eee4fe4fdf4_84727136',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '32f79100c4c860cbb4be3dd29f634561743939a4' => 
     array (
       0 => '/opt/lampp/htdocs/carpeta/leo-imdb/templates/detailMovies.tpl',
-      1 => 1665064242,
+      1 => 1665068621,
       2 => 'file',
     ),
   ),
@@ -20,18 +20,55 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_633edd351bb209_52858770 (Smarty_Internal_Template $_smarty_tpl) {
+function content_633eee4fe4fdf4_84727136 (Smarty_Internal_Template $_smarty_tpl) {
 ?><h1><?php echo $_smarty_tpl->tpl_vars['detalles']->value;?>
 </h1>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+
 <ul class="list-group">
-   <li><?php echo $_smarty_tpl->tpl_vars['moviesDetail']->value->nombre;?>
+<?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['details']->value, 'detail');
+$_smarty_tpl->tpl_vars['detail']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['detail']->value) {
+$_smarty_tpl->tpl_vars['detail']->do_else = false;
+?>
+      <li class="list-group-item list-group-item-success">Nombre: <?php echo $_smarty_tpl->tpl_vars['detail']->value->nombre;?>
 </li>
-   <li><?php echo $_smarty_tpl->tpl_vars['moviesDetail']->value->anio;?>
+      <li class="list-group-item list-group-item-success">Fecha de lanzamiento: <?php echo $_smarty_tpl->tpl_vars['detail']->value->anio;?>
 </li>
-   <li><?php echo $_smarty_tpl->tpl_vars['moviesDetail']->value->produccion;?>
+      <li class="list-group-item list-group-item-success">Costo de produccion: <?php echo $_smarty_tpl->tpl_vars['detail']->value->produccion;?>
 </li>
-   <li><?php echo $_smarty_tpl->tpl_vars['moviesDetail']->value->recaudacion;?>
-</li>
-</ul><?php }
+      <li class="list-group-item list-group-item-success">Recaudacion: <?php echo $_smarty_tpl->tpl_vars['detail']->value->recaudacion;?>
+</li>     
+       
+<?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+</ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php }
 }
