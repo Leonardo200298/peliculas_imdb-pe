bc8f6478ecbde2a1c5219ce3231d011d6c9b6da4
Leonardo200298 <leonardosanchez119@gmail.com>
2022-10-22 19:38:36 -0300

    Feat:generosController modified, add new instance of auth in admin functions

--- controller/generosController.php
+++ controller/generosController.php
@@ -1,48 +1,50 @@
 <?php
 require_once './model/generosModel.php';
 require_once './view/generosView.php';
-require_once './model/peliculasModel.php';
+require_once './helpers/auth.helper.php';
 
-class generosController{
+class generosController
+{
     private $model;
     private $view;
-    private $movieModel;
+    private $auth;
     function __construct()
     {
         $this->model = new generosModel();
         $this->view = new generosView();
-        $this->movieModel = new peliculasModel();
-
+        $this->auth = new AuthHelper();
     }
-    function obtenerGeneros(){
-        $generos=$this-> model->obtenerGeneros();
+    function obtenerGeneros()
+    {
+        $generos = $this->model->obtenerGeneros();
         $this->view->mostrarGeneros($generos);
-        
     }
-    public function agregarGenero(){
+    public function agregarGenero()
+    {
+        $this->auth->checkLoggedIn();
         $nombreGenre = $_POST['nombre'];
         $this->model->crearGeneroDb($nombreGenre);
-        header('Location: '. BASE_URL);
+        header('Location: ' . BASE_URL);
     }
 
-    function BorrarGenero($id_genero){
+    function BorrarGenero($id_genero)
+    {
+        $this->auth->checkLoggedIn();
         $pelis = $this->movieModel->peliculasConEseGenero($id_genero);
         if ($pelis = null) {
             $this->model->EliminarGenero($id_genero);
-            header('Location: '. BASE_URL);
-        }
-        else {
+            header('Location: ' . BASE_URL);
+        } else {
             $this->view->error("Este genero no se puede eliminar");
         }
-
-
     }
-    public function editarGenero($id){
+    public function editarGenero($id)
+    {
+        $this->auth->checkLoggedIn();
         $nombre = $_POST['nombre'];
         $genero = $_POST['genero'];
 
-        $this->model->editarModel($nombre,$genero);
-        header('Location: '. BASE_URL);
+        $this->model->editarModel($nombre, $genero);
+        header('Location: ' . BASE_URL);
     }
-  
 }
